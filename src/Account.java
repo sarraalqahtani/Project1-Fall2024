f4f68ff8788b70825f719222d33666678a8d5658
alqahtas <alqahtas@wfu.edu>
2024-09-02 17:12:23 -0400

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Account {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+    private ArrayList<Transaction> transactions;
+
+    // Constructor
+    public Account(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+        this.transactions = new ArrayList<>();
+    }
+
+    // Getters and Setters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+    // Methods to add and manage transactions
+}
---
+++ src/BankingSystem.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class BankingSystem {
+    private static ArrayList<Account> accounts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        // Example command line input: java BankingSystem create JohnDoe 12345 1000.0
+        // Example command line input: java BankingSystem deposit 12345 500.0
+        // Example command line input: java BankingSystem withdraw 12345 200.0
+        // Example command line input: java BankingSystem balance 12345
+
+
+    }
+
+  }
